e07ff53ebf1706e607f197207802cbb0ac3c4f6d
Ian Tricker <67514513+IanTricker@users.noreply.github.com>
2025-04-15 15:46:19 +0000

    Added cards

--- Card.java
+++ Card.java
@@ -3,23 +3,56 @@ import java.io.*;
 
 class Card{
 
-  public Card(){
-  int NUMCARDS = 52;
-  String[] RANKNAME = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-  String[] FILE_RANK_NAME = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
-  String[] SUITNAME = {"clubs", "hearts", "spades", "diamonds"};
-  String[] STATE = {"deck", "player", "dealer", "discard"};
+  ArrayList<Integer> cards = new ArrayList<>();
 
-  int DECK = 0;
-  int player = 1;
-  int dealer = 2;
-  int discard = 3;
+  public Card(){
+    
   } // end constructor
 
   public static void main(String[] args){
-    System.out.print(NUMCARDS);
+    Card ca = new Card();
+    ca.test();
   } // end main
 
+  public void test(){
+    setUpCards();
+    Random random = new Random();
+    int randomNumber = random.nextInt(52);
+    System.out.println(inCards(randomNumber));
+  } // end test
 
+  public String inCards(int cardNum){
+    int NUMCARDS = 52;
+    String[] RANKNAME = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    String[] FILE_RANK_NAME = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
+    String[] SUITNAME = {"clubs", "hearts", "spades", "diamonds"};
+    String[] STATE = {"deck", "player", "dealer", "discard"};
 
+    int DECK = 0;
+    int player = 1;
+    int dealer = 2;
+    int discard = 3;
+
+    int id = cardNum;
+    int suit = Math.floorDiv(cardNum, 13);
+    int rank = cardNum % 13;
+
+    String suitName = SUITNAME[suit];
+    String rankName = RANKNAME[rank];
+
+    int state = DECK;
+    int value = rank;
+    
+    String cardName = suitName + " " + rankName;
+
+    return cardName;
+  } // end inCards
+
+  
+
+  public void setUpCards(){
+    for(int i = 0; i < 52; i++){
+      cards.add(0);
+    } // end for
+  } // end setUpCards
 } // end card class
